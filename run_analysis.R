dae519c9c3aff8528f2be5baae1009dd0af56d3a
DylanLennard <dlennard09@yahoo.com>
2016-12-03 22:21:30 -0800

    Modified the formatting a little to match google's best practices suggestions for R

--- run_analysis.R
+++ run_analysis.R
@@ -1,93 +1,100 @@
-#setwd('your path name here')
-#Install dplyr if not already installed on computer, then load it 
+################################################################################
+############################# run_analysis.R ###################################
+### Constructs datasets for cell phone data from Samsung and export the data 
+### into a csv file to be easily read in. This was part of a project for the 
+### Coursera Getting and Cleaning Data course from JHU
+################################################################################
+
+# setwd("Your directory here if you want to specify it")
+
 if (!require("data.table")) {
-    install.packages("data.table")
-    library(data.table)
+  install.packages("data.table")
+  library(data.table)
 }
 
-#Create the project directory
+construct_data<- function (string){
+  # construct datasets based on string
+  #
+  # Args: 
+  #   string: string indicating whether test or training set
+  #
+  # Returns:
+  #   DT: a data.table of test or training data, depending on string entered
+  df_file <- paste0('./project/UCI_HAR_Dataset/', string,
+                    '/X_', string,'.txt')
+    
+  action_file <- paste0('./project/UCI_HAR_Dataset/', string, 
+                        '/Y_', string, '.txt')
+    
+  subject_file <- paste0('./project/UCI_HAR_Dataset/', string, 
+                         '/subject_', string, '.txt')
+    
+  DT <- fread(df_file, sep = ' ')
+  DT$subject <- fread(subject_file, sep=' ')
+  DT$action <- fread(action_file, sep=' ')
+  setnames(DT, c(features, "subject", "action"))
+    
+  return (DT)
+}
+
+# Create the project directory
 if (!dir.exists('project')){dir.create('project')}
 
 #if directory empty, download zip file and unzip it
-#fix this to look for the actual zip file
 if (length(dir('./project')) == 0){
-    projectURL <- paste0('https://d396qusza40orc.cloudfront.net/',
-                     'getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip')
-    download.file(projectURL, destfile= './project/project.zip', 
-                  method='curl')
-    datedownloaded = date()
+    
+  projectURL <- paste0('https://d396qusza40orc.cloudfront.net/',
+                       'getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip')
+  download.file(projectURL, destfile= './project/project.zip', 
+                method='curl')
+  datedownloaded = date()
 
-    #unzip the contents & download the dataset.
-    unzip('./project/project.zip', list=FALSE, exdir = './project')
+  # unzip the contents & download the dataset.
+  unzip('./project/project.zip', list=FALSE, exdir = './project')
 
-    #rename file
-    file.rename('./project/UCI HAR Dataset', './project/UCI_HAR_Dataset')
+  # rename file
+  file.rename('./project/UCI HAR Dataset', './project/UCI_HAR_Dataset')
 }
 
-#Uncomment to see what's in file
-#list.files('./project/UCI_HAR_Dataset')
+# Uncomment to see what's in file
+# list.files('./project/UCI_HAR_Dataset')
 
-###############Step 1: Merge datasets############################
-#pull in the features.txt info for labels
-# features <- read_delim('./project/UCI_HAR_Dataset/features.txt',
-#                        delim = ' ', col_names = FALSE)[,2]
-
+# pull in the features.txt info for labels
 features <- read.table("./project/UCI_HAR_Dataset/features.txt", 
                        sep = " ", colClasses = "character")[,2]
 
-#construct each dataset by pulling in the X datasets with "features" as the
-#column names, and then merge y and subject vectors
-construct_data<- function (string){
-    df_file <- paste0('./project/UCI_HAR_Dataset/', string,
-                      '/X_', string,'.txt')
-    action_file <- paste0('./project/UCI_HAR_Dataset/', string, 
-                          '/Y_', string, '.txt')
-    subject_file <- paste0('./project/UCI_HAR_Dataset/', string, 
-                           '/subject_', string, '.txt')
-    
-    DT <- fread(df_file, sep = ' ')
-    DT$subject <- fread(subject_file, sep=' ')
-    DT$action <- fread(action_file, sep=' ')
-    setnames(DT, c(features, "subject", "action"))
-    
-    return (DT)
-}
-
+# construct each dataset 
 testDT <- construct_data('test')
 trainDT <- construct_data('train')
 
-#append the datasets on proper variables
+# append the datasets on proper variables
 appendedDT <- rbindlist(list(testDT, trainDT))
 
 rm(testDT, trainDT, features)
 
-##############################Step 2##########################################
-
+# clean up the dt_names
 dt_names <- grep('mean|std', names(appendedDT), 
                  value=TRUE, ignore.case = TRUE)
 dt_names <- append(dt_names, c('subject', 'action'))
 
-#use df_names to select those variables, switch to data.table notation 
+# use dt_names to select those variables, switch to data.table notation 
 appendedDT <- appendedDT[, dt_names, with=FALSE]
 
 rm(dt_names)
 
-#############################Step 3##########################################
-#use activity_labels.txt to match numbers in dataset to the activities 
+# use activity_labels.txt to match numbers in dataset to the activities 
 activity <- read.table('./project/UCI_HAR_Dataset/activity_labels.txt',
-                      sep='', col.names = c("action", "activity_performed"))
+                       sep='', col.names = c("action", "activity_performed"))
 
 mergedDT <- merge(appendedDT, activity, by='action')
 
 rm(activity, appendedDT)
 
-############################Step 4############################################
-#Loop through and replace various components of the variable names rather than
-#perform them manually. 
+# Loop through and replace various components of the variable names rather than
+# perform them manually. 
   
 merged_names <- names(mergedDT)
 
-# possibly also substitute hyphens with underscores, or nothing 
 keywords <- c('^t', '^f', '\\.*X', '\\.*Y', '\\.*Z', '\\.', 'mean','std','-')
 strings <- c('time', 'freq', 'X', 'Y', 'Z', '', 'Mean', 'StdDev', '_')
 
@@ -95,23 +102,21 @@ for (i in 1:length(keywords)){
     merged_names <- gsub(keywords[i], strings[i], merged_names)
 }
 
-#change original names of DT (double check this)
+# change original names of DT (double check this)
 setnames(mergedDT, merged_names) 
 
-############################Step 5############################################
-#group by activity and subject, and report the mean for each variable. 
+# group by activity and subject, and report the mean for each variable. 
 DT <- mergedDT[, lapply(.SD, mean), by=c('subject', 'action')]
 
-#Finally, write this dataset out into a .txt file. 
+# Finally, write this dataset out into a .txt file. 
 write.table(DT, "MeanData.txt", sep=' ', row.names = FALSE)
 
 
-######################Removing files and Objects###############################
-#remove objects in memory
+# remove objects in memory
 rm(list = ls())
 
-#if you want to delete the project folder after running, 
-#uncomment the following line:
-#unlink('./project', recursive = TRUE)
+# if you want to delete the project folder after running, 
+# uncomment the following line:
+# unlink('./project', recursive = TRUE)
 
 ##############################################################################
